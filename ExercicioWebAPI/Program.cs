44e23193264ab6b247a5a3e121b8f1faeaa548fd
vitto.mazeto <vittopicma@gmail.com>
2023-06-15 17:53:16 -0300

    Commit teste

--- ExercicioWebAPI/Program.cs
+++ ExercicioWebAPI/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+Console.WriteLine("Teste");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
